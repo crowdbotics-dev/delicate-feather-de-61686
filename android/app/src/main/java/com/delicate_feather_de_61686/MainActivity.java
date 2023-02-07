50fa2ea80bf398eb63b1684eaf8e4b1f9ff6da65
Crowdbotics <engineering@crowdbotics.com>
2023-02-07 03:19:34 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/delicate_feather_de_61686/MainActivity.java
@@ -0,0 +1,15 @@
+package com.delicate_feather_de_61686;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "delicate_feather_de_61686";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "delicate-feather-de-61686",
+  url: "https://delicate-feather-de-61686.botics.co",
+  api: "https://delicate-feather-de-61686.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
